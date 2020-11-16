7417a097525d87920b2218fed3e16ec9a0fc59bb
mariusu-msft <58800196+mariusu-msft@users.noreply.github.com>
2020-11-16 06:31:39 -0800

    Add gitignore for pods

---
+++ Xamarin.AzureCommunicationCalling.iOS/nativeLibs/nativeLibsPlaceholder/nativeLibsPlaceholder/nativeLibsPlaceholder.h
@@ -0,0 +1,18 @@
+//
+//  nativeLibsPlaceholder.h
+//  nativeLibsPlaceholder
+//
+//  Created by Marius Ungureanu on 11/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for nativeLibsPlaceholder.
+FOUNDATION_EXPORT double nativeLibsPlaceholderVersionNumber;
+
+//! Project version string for nativeLibsPlaceholder.
+FOUNDATION_EXPORT const unsigned char nativeLibsPlaceholderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <nativeLibsPlaceholder/PublicHeader.h>
+
+
