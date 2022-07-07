196e982fb52e3bd660cf8c585b9f212681d066a3
Thomas Haukland <thomas.haukland@gmail.com>
2022-07-07 10:47:35 +0200

    Added methods for call: accept/reject.
    https://github.com/Laerdal/Xamarin.AzureCommunicationCalling/issues/32

--- Bindings/Android/AzureCommunicationCallingHelper/src/main/java/com/laerdal/azurecommunicationhelper/CallClientHelper.java
+++ Bindings/Android/AzureCommunicationCallingHelper/src/main/java/com/laerdal/azurecommunicationhelper/CallClientHelper.java
@@ -1,6 +1,8 @@
 package com.laerdal.azurecommunicationhelper;
 
 import android.content.Context;
+
+import com.azure.android.communication.calling.AcceptCallOptions;
 import com.azure.android.communication.calling.Call;
 import com.azure.android.communication.calling.CallAgent;
 import com.azure.android.communication.calling.CallAgentOptions;
@@ -8,6 +10,7 @@ import com.azure.android.communication.calling.CallClient;
 import com.azure.android.communication.calling.CallInfo;
 import com.azure.android.communication.calling.DeviceManager;
 import com.azure.android.communication.calling.HangUpOptions;
+import com.azure.android.communication.calling.IncomingCall;
 import com.azure.android.communication.calling.LocalVideoStream;
 import com.azure.android.communication.calling.StartCallOptions;
 import com.azure.android.communication.calling.VideoDeviceInfo;
@@ -64,6 +67,12 @@ public class CallClientHelper {
     }
     public static void StopVideo(Call call, LocalVideoStream localVideoStream, Context context) throws ExecutionException, InterruptedException {
         call.stopVideo(context, localVideoStream).get();
+    }
+    public static Call Accept(IncomingCall incomingCall, AcceptCallOptions acceptCallOptions, Context context) throws ExecutionException, InterruptedException {
+        return incomingCall.accept(context, acceptCallOptions).get();
+    }
+    public static void Reject(IncomingCall incomingCall) throws ExecutionException, InterruptedException {
+        incomingCall.reject().get();
     }
 }
 
