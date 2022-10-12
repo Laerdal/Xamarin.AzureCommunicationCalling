a40713f69edf14276fff2378b5d47f5cbaf74e49
Anderson Lopes <andersonvieira123456@gmail.com>
2022-10-12 11:14:10 -0300

    Screensharing for Android

--- Bindings/Android/AzureCommunicationCallingHelper/src/main/java/com/laerdal/azurecommunicationhelper/CallClientHelper.java
+++ Bindings/Android/AzureCommunicationCallingHelper/src/main/java/com/laerdal/azurecommunicationhelper/CallClientHelper.java
@@ -7,17 +7,25 @@ import com.azure.android.communication.calling.Call;
 import com.azure.android.communication.calling.CallAgent;
 import com.azure.android.communication.calling.CallAgentOptions;
 import com.azure.android.communication.calling.CallClient;
+import com.azure.android.communication.calling.CallFeature;
 import com.azure.android.communication.calling.CallInfo;
 import com.azure.android.communication.calling.DeviceManager;
+import com.azure.android.communication.calling.Features;
+import com.azure.android.communication.calling.FrameConfirmation;
 import com.azure.android.communication.calling.HangUpOptions;
+import com.azure.android.communication.calling.HardwareBasedVideoFrameSender;
 import com.azure.android.communication.calling.IncomingCall;
 import com.azure.android.communication.calling.LocalVideoStream;
+import com.azure.android.communication.calling.OutgoingVideoStream;
 import com.azure.android.communication.calling.PushNotificationInfo;
+import com.azure.android.communication.calling.SoftwareBasedVideoFrameSender;
 import com.azure.android.communication.calling.StartCallOptions;
 import com.azure.android.communication.calling.VideoDeviceInfo;
+import com.azure.android.communication.calling.VideoFrameSender;
 import com.azure.android.communication.common.CommunicationIdentifier;
 import com.azure.android.communication.common.CommunicationTokenCredential;
 
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -36,6 +44,7 @@ public class CallClientHelper {
         return callClient.createCallAgent(context, credentials, callAgentOptions).get();
     }
     public static void HangUp(Call call, HangUpOptions options) throws ExecutionException, InterruptedException {
+
         call.hangUp(options).get();
     }
     public static Call Call(
@@ -66,8 +75,18 @@ public class CallClientHelper {
     public static void StartVideo(Call call, LocalVideoStream localVideoStream, Context context) throws ExecutionException, InterruptedException {
         call.startVideo(context, localVideoStream).get();
     }
+    public static void StartScreenShare(Call call, OutgoingVideoStream outgoingVideoStream, Context context) throws ExecutionException, InterruptedException {
+        call.startVideo(context, outgoingVideoStream).get();
+    }
+    public static void FillFrame(ByteBuffer plane, int bandBegin, int bandsCount, byte greyValue) throws ExecutionException, InterruptedException {
+        java.util.Arrays.fill(plane.array(), bandBegin, bandsCount, greyValue);
+
+    }
     public static void StopVideo(Call call, LocalVideoStream localVideoStream, Context context) throws ExecutionException, InterruptedException {
         call.stopVideo(context, localVideoStream).get();
+    }
+    public static void StopScreenShare(Call call, OutgoingVideoStream outgoingVideoStream, Context context) throws ExecutionException, InterruptedException {
+        call.stopVideo(context, outgoingVideoStream).get();
     }
     public static Call Accept(IncomingCall incomingCall, AcceptCallOptions acceptCallOptions, Context context) throws ExecutionException, InterruptedException {
         return incomingCall.accept(context, acceptCallOptions).get();
@@ -83,6 +102,12 @@ public class CallClientHelper {
     }
     public static void HandlePushNotification(CallAgent callAgent, PushNotificationInfo pushNotificationInfo) throws ExecutionException, InterruptedException {
         callAgent.handlePushNotification(pushNotificationInfo).get();
+    }
+    public static FrameConfirmation ScreensharingVideoFrameSender(SoftwareBasedVideoFrameSender softwareBasedVideoFrameSender, ByteBuffer byteBuffer, long timeStamp) throws ExecutionException, InterruptedException {
+         return softwareBasedVideoFrameSender.sendFrame(byteBuffer, timeStamp).get();
+    }
+    public static FrameConfirmation ScreensharingVideoFrameSender(HardwareBasedVideoFrameSender hardwareBasedVideoFrameSender, int targetId, int[] textureIds, long timeStamp) throws ExecutionException, InterruptedException {
+         return hardwareBasedVideoFrameSender.sendFrame(targetId, textureIds[0], timeStamp).get();
     }
 }
 
