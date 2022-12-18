41a748aa37717b18d502def729ad46ecf8f59cac
Anderson Lopes <andersonvieira123456@gmail.com>
2022-12-18 11:24:02 -0300

    Add class ScreenSharingDelegate

---
+++ TestSample/TestSample.iOS/Implementations/AzureCallback/ScreenSharingDelegate.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.AzureCommunicationCalling.iOS;
+
+namespace TestSample.iOS.Implementations
+{
+    public class ScreenSharingDelegate : ACSRawOutgoingVideoStreamOptionsDelegate
+    {
+        private readonly CallingCallbackManager _videoCallbackManager;
+        private Action<ACSRawOutgoingVideoStreamOptions, ACSOutgoingVideoStreamStateChangedEventArgs> remoteParticipants;
+        public static ACSVideoFrameSender aCSVideoFrameSender;
+        public ScreenSharingDelegate(CallingCallbackManager videoCallbackManager)
+        {
+            _videoCallbackManager = videoCallbackManager;
+        }
+
+        public override void OnOutgoingVideoStreamStateChanged(ACSRawOutgoingVideoStreamOptions rawOutgoingVideoStreamOptions, ACSOutgoingVideoStreamStateChangedEventArgs args)
+        {
+            _videoCallbackManager.ScreenSharing?.Invoke(rawOutgoingVideoStreamOptions, args);
+        }
+        public override void OnVideoFrameSenderChanged(ACSRawOutgoingVideoStreamOptions rawOutgoingVideoStreamOptions, ACSVideoFrameSenderChangedEventArgs args)
+        {
+            aCSVideoFrameSender = args.VideoFrameSender;
+        }
+    }
+}
