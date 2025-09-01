27dd0ef413059bdfe884bf90072c94a2c69c4b73
Marcin Klimczak <marcin@MacBook-Pro-Marcin.local>
2025-09-01 16:08:16 +0200

    add build script

--- Bindings/Android/AzureCommunicationCallingHelper/settings.gradle.kts
+++ Bindings/Android/AzureCommunicationCallingHelper/settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "My Application"
+rootProject.name = "AzureCommunicationHelper"
 // include(":app")
 include(":AzureCommunicationHelper")
